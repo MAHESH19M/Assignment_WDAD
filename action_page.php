648b6526ca2f55bba14cce38c11357f1c8278962
Maheshram Shunmuganand <smaheshram@yahoo.com>
2021-09-24 03:10:58 +0530

    Add logo, repeat links, form and more

---
+++ action_page.php
@@ -0,0 +1,4 @@
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Your email date-of-birth is: <?php echo $_POST["dob"]; ?>
+Product intrest is: <?php echo $_POST["iter"]; ?>
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function validateForm() {
     var name = document.forms["myForm"]["name"].value;
     var email = document.forms["myForm"]["email"].value;
+    var dob = document.forms["myForm"]["dob"].value;
+    var iter = document.forms["myForm"]["iter"].value;
 
     if (name == "") {
         alert("Name must be filled out");
@@ -8,6 +10,14 @@ function validateForm() {
     }
     if (email == "") {
         alert("Email must be filled out");
+        return false;
+    }
+    if (dob == "") {
+        alert("Date of birth must be filled out");
+        return false;
+    }
+    if (iter == "") {
+        alert("Product interest must be filled out");
         return false;
     }
     
